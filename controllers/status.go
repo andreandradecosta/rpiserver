29e642d168eee28a52078c125d579b62f9a705e9
Andre Costa <andre.andrade.costa@gmail.com>
2016-03-19 15:26:57 -0300

    Uptime method added

--- controllers/status.go
+++ controllers/status.go
@@ -22,6 +22,10 @@ func NewStatus(renderer *render.Render, router *mux.Router, redisPool *redis.Poo
 		Path("/").
 		Name("Index").
 		Handler(s.handleAction(s.index))
+	router.
+		Methods("GET").
+		Path("/uptime").
+		Handler(s.handleAction(s.uptime))
 }
 
 func (s *status) index(w http.ResponseWriter, r *http.Request) error {
@@ -40,5 +44,24 @@ func (s *status) index(w http.ResponseWriter, r *http.Request) error {
 	delete(d, "disk_part")
 	delete(d, "host")
 	s.JSON(w, http.StatusOK, d)
+	return nil
+}
+
+func (s *status) uptime(w http.ResponseWriter, r *http.Request) error {
+	conn := s.redisPool.Get()
+	defer conn.Close()
+	b, err := redis.Bytes(conn.Do("GET", "status"))
+	if err != nil {
+		return err
+	}
+
+	var d models.Info
+	err = json.Unmarshal(b, &d)
+	if err != nil {
+		return nil
+	}
+	host := d["host"].(map[string]interface{})
+	res := host["uptime"].(float64) / (60 * 60 * 24)
+	s.JSON(w, http.StatusOK, res)
 	return nil
 }
