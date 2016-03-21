b4d33858974ab3221a442441fdc1778e738fb268
Andre Costa <andre.andrade.costa@gmail.com>
2016-03-21 15:24:27 -0300

    Returning JSON from uptime and new method for updated.

--- controllers/status.go
+++ controllers/status.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/andreandradecosta/rpimonitor/models"
 	"github.com/garyburd/redigo/redis"
@@ -26,6 +27,10 @@ func NewStatus(renderer *render.Render, router *mux.Router, redisPool *redis.Poo
 		Methods("GET").
 		Path("/uptime").
 		Handler(s.handleAction(s.uptime))
+	router.
+		Methods("GET").
+		Path("/updated").
+		Handler(s.handleAction(s.updated))
 }
 
 func (s *status) index(w http.ResponseWriter, r *http.Request) error {
@@ -61,7 +66,25 @@ func (s *status) uptime(w http.ResponseWriter, r *http.Request) error {
 		return nil
 	}
 	host := d["host"].(map[string]interface{})
-	res := host["uptime"].(float64) / (60 * 60 * 24)
+	days := host["uptime"].(float64) / (60 * 60 * 24)
+	res := map[string]float64{
+		"uptime": days,
+	}
+	s.JSON(w, http.StatusOK, res)
+	return nil
+}
+
+func (s *status) updated(w http.ResponseWriter, r *http.Request) error {
+	conn := s.redisPool.Get()
+	defer conn.Close()
+	up, err := redis.Int64(conn.Do("GET", "updated"))
+	if err != nil {
+		return err
+	}
+	date := time.Unix(up, 0)
+	res := map[string]time.Time{
+		"updated": date,
+	}
 	s.JSON(w, http.StatusOK, res)
 	return nil
 }
