8ebc64d1d5f30dfae9c25675740d6593f28f320a
Andre Costa <andre.andrade.costa@gmail.com>
2016-03-29 11:38:12 -0300

    New status response.

--- controllers/status.go
+++ controllers/status.go
@@ -23,68 +23,37 @@ func NewStatus(renderer *render.Render, router *mux.Router, redisPool *redis.Poo
 		Path("/").
 		Name("Index").
 		Handler(s.handleAction(s.index))
-	router.
-		Methods("GET").
-		Path("/uptime").
-		Handler(s.handleAction(s.uptime))
-	router.
-		Methods("GET").
-		Path("/updated").
-		Handler(s.handleAction(s.updated))
 }
 
 func (s *status) index(w http.ResponseWriter, r *http.Request) error {
 	conn := s.redisPool.Get()
 	defer conn.Close()
-	b, err := redis.Bytes(conn.Do("GET", "status"))
+	reply, err := redis.Values(conn.Do("MGET", "status", "updated"))
 	if err != nil {
 		return err
 	}
-	var d models.Info
-	err = json.Unmarshal(b, &d)
-	if err != nil {
+	var status []byte
+	var updated int64
+	if _, err = redis.Scan(reply, &status, &updated); err != nil {
 		return err
 	}
 
-	delete(d, "disk_part")
-	delete(d, "host")
-	s.JSON(w, http.StatusOK, d)
-	return nil
-}
-
-func (s *status) uptime(w http.ResponseWriter, r *http.Request) error {
-	conn := s.redisPool.Get()
-	defer conn.Close()
-	b, err := redis.Bytes(conn.Do("GET", "status"))
+	var info models.Info
+	err = json.Unmarshal(status, &info)
 	if err != nil {
 		return err
 	}
+	response := make(models.Info)
 
-	var d models.Info
-	err = json.Unmarshal(b, &d)
-	if err != nil {
-		return nil
-	}
-	host := d["host"].(map[string]interface{})
+	host := info["host"].(map[string]interface{})
 	days := host["uptime"].(float64) / (60 * 60 * 24)
-	res := map[string]float64{
-		"uptime": days,
-	}
-	s.JSON(w, http.StatusOK, res)
-	return nil
-}
+	response["uptime"] = days
 
-func (s *status) updated(w http.ResponseWriter, r *http.Request) error {
-	conn := s.redisPool.Get()
-	defer conn.Close()
-	up, err := redis.Int64(conn.Do("GET", "updated"))
-	if err != nil {
-		return err
-	}
-	date := time.Unix(up, 0).In(s.hostLocation)
-	res := map[string]time.Time{
-		"updated": date,
-	}
-	s.JSON(w, http.StatusOK, res)
+	date := time.Unix(updated, 0).In(s.hostLocation)
+	response["updated"] = date
+
+	response["users"] = info["users"]
+
+	s.JSON(w, http.StatusOK, response)
 	return nil
 }
